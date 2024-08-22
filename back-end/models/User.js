5aa35ee5678bc2a05339162fc40a09d70aac8dd4
Lucas Silva <lucasyannsantossilva@hotmail.com>
2024-08-22 20:55:11 -0300

    Adicionando API - Cadastro

--- back-end/models/User.js
+++ back-end/models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const moment = require('moment'); 
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -13,6 +14,18 @@ const userSchema = new mongoose.Schema({
     password: {
         type: String,
         required: true
+    },
+    birthdate: {
+        type: Date,
+        required: true,
+        validate: {
+            validator: function(value) {
+                // Calcular a idade com base na data de nascimento
+                const age = moment().diff(value, 'years');
+                return age >= 18; // Retorna true se a idade for 18 ou mais
+            },
+            message: 'Você deve ter pelo menos 18 anos de idade.'
+        }
     }
 });
 
---
+++ back-end/routes/users.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const router = express.Router();
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const User = require('../models/User');
+
+// Rota para cadastro de usuário
+router.post('/register', async (req, res) => {
+    const { name, email, password, birthdate} = req.body;
+
+    if (!name) {
+        return res.status(400).json({ msg: 'O nome é obrigatório.' });
+    }
+    if (!email) {
+        return res.status(400).json({ msg: 'O email é obrigatório.' });
+    }
+    if (!password) {
+        return res.status(400).json({ msg: 'A senha é obrigatória.' });
+    }
+    if (!birthdate) {
+        return res.status(400).json({ msg: 'A data de nascimento é obrigatória.' });
+    }
+
+    try {
+        let user = await User.findOne({ email });
+        if (user) {
+            return res.status(400).json({ msg: 'Usuário já existe' });
+        }
+
+        user = new User({
+            name,
+            email,
+            password: await bcrypt.hash(password, 10), // Hash da senha
+            birthdate
+        });
+
+        await user.save();
+        res.status(201).json({ msg: 'Usuário cadastrado com sucesso' });
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Erro no servidor');
+    }
+});
+
+
+module.exports = router;
