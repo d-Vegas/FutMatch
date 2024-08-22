35710974b2cc1d2702ed2533f7a522eb97b4b61c
Lucas Silva <lucasyannsantossilva@hotmail.com>
2024-08-22 19:43:53 -0300

    Criando o banco de dados / Conectando com o MongoDB

--- back-end/server.js
+++ back-end/server.js
@@ -16,5 +16,19 @@ app.get('/', (req, res) =>{
     res.status(200).json({ msg: 'Bem Vindo a minha API'})
 })
 
+//Crendenciais
+const dbUser = process.env.DB_USER
+const dbPassword = process.env.DB_PASS
+
+mongoose
+    .connect(`
+        mongodb+srv://${dbUser}:${dbPassword}@cluster0.ldify.mongodb.net/FutMatch`
+    )
+    .then(() =>{
+        app.listen(3000)
+        console.log('Conectou ao banco!')
+    })
+    .catch((err) => console.log(err))
+
 app.listen(3000)
 
