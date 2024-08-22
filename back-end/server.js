d5dcfe1869b48c4d71535fe595dfd4c58796e68e
Lucas Silva <lucasyannsantossilva@hotmail.com>
2024-08-22 20:23:20 -0300

    Definindo e Criando a Model

--- back-end/server.js
+++ back-end/server.js
@@ -1,34 +1,33 @@
-require('dotenv').config()
+require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const userRoutes = require('./routes/users');
-const jwt = require('jsonwebtoken')
-const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get('/', (req, res) =>{
-    res.status(200).json({ msg: 'Bem Vindo a minha API'})
-})
+// Definindo as rotas de usuário
+app.use('/api/users', userRoutes);
 
-//Crendenciais
-const dbUser = process.env.DB_USER
-const dbPassword = process.env.DB_PASS
+app.get('/', (req, res) => {
+    res.status(200).json({ msg: 'Bem Vindo à minha API!' });
+});
+
+// Credenciais
+const dbUser = process.env.DB_USER;
+const dbPassword = process.env.DB_PASS;
 
 mongoose
-    .connect(`
-        mongodb+srv://${dbUser}:${dbPassword}@cluster0.ldify.mongodb.net/FutMatch`
-    )
-    .then(() =>{
-        app.listen(3000)
-        console.log('Conectou ao banco!')
+    .connect(`mongodb+srv://${dbUser}:${dbPassword}@cluster0.ldify.mongodb.net/FutMatch`)
+    .then(() => {
+        app.listen(3000, () => {
+            console.log('Conectado ao banco e servidor rodando na porta 3000');
+        });
     })
-    .catch((err) => console.log(err))
-
-app.listen(3000)
-
+    .catch((err) => console.log(err));
